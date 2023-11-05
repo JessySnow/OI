bbf013a4a16c1b039935187d2320f1f37cbff166
JessySnow <jessysnow12345@gmail.com>
2023-11-05 17:10:41 +0800

    feat: add AdjList and AdjMatrix for graph storage

---
+++ graph/src/acgn/jessysnow/storage/AdjList.java
@@ -0,0 +1,95 @@
+package acgn.jessysnow.storage;
+
+import java.util.*;
+
+/**
+ * 邻接表实现
+ */
+public class AdjList {
+
+    /**
+     * 查找边是否存在, O(n)
+     *  O(n) 在一个点和其他所有的点都存在关联时需要遍历 n 次
+     */
+    private static boolean findEdge(List<List<Integer>> adjList, int from, int to) {
+        if (null == adjList.get(from)) {
+            return false;
+        }
+
+        for (Integer i : adjList.get(from)) {
+            if (to == i) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+
+    /**
+     * 优化后的查找边是否存在，需要邻接表对出边进行排序, O(logn)
+     *  二分搜索可以从 O(n) 优化至 O(logn)
+     */
+    // FIXME 二分搜索的逻辑存在错误
+    private static boolean findEdgeOptimized(List<List<Integer>> adjList, int from, int to) {
+        List<Integer> fromTo = adjList.get(from);
+        if (null == fromTo) {
+            return false;
+        }
+
+        int left = 0, right = fromTo.size() - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (fromTo.get(mid) == to) {
+                return true;
+            } else if (fromTo.get(mid) > to) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * 深度优先搜索
+     */
+    private static List<Integer> dfs(List<List<Integer>> adjList, int from, Set<Integer> visited) {
+        if (visited.contains(from) || null == adjList.get(from)) {
+            return Collections.emptyList();
+        }
+
+        List<Integer> res = new ArrayList<>();
+        for (Integer to : adjList.get(from)) {
+            res.add(from);
+            res.add(to);
+            visited.add(from);
+            res.addAll(dfs(adjList, to, visited));
+        }
+
+        return res;
+    }
+
+
+    public static void main(String[] args) {
+        List<List<Integer>> adjList = new ArrayList<>();
+        ArrayList<Integer> fromZero = new ArrayList<>();
+        fromZero.add(1);
+        ArrayList<Integer> fromOne = new ArrayList<>();
+        fromOne.add(2);
+        ArrayList<Integer> fromTwo = new ArrayList<>();
+        fromTwo.add(3);
+        ArrayList<Integer> fromThree = new ArrayList<>();
+
+        adjList.add(fromZero);
+        adjList.add(fromOne);
+        adjList.add(fromTwo);
+        adjList.add(fromThree);
+
+        System.out.println(findEdge(adjList, 1, 2));
+        System.out.println(findEdgeOptimized(adjList, 1, 2));
+
+        System.out.println(dfs(adjList, 0, new HashSet<>()));
+    }
+}
---
+++ graph/src/acgn/jessysnow/storage/AdjMatrix.java
@@ -0,0 +1,55 @@
+package acgn.jessysnow.storage;
+
+import java.util.*;
+
+/**
+ * 邻接矩阵实现
+ */
+public class AdjMatrix {
+
+    /**
+     * 查找边，O(1)
+     */
+    private static boolean findEdge(Integer[][] adjMatrix, int from, int to) {
+        return adjMatrix[from][to] == 1;
+    }
+
+    /**
+     * 深搜，O(n^2)
+     *  O(n) 内层遍历
+     *  O(n) 外层递归
+     */
+    private static List<Integer> dfs(Integer[][] adjMatrix, int from, Set<Integer> visited) {
+        if (visited.contains(from)) {
+            return Collections.emptyList();
+        }
+
+        List<Integer> res = new ArrayList<>();
+        for (int i = 0; i < adjMatrix[from].length; ++i) {
+            if (1 == adjMatrix[from][i]) {
+                res.add(from);
+                res.add(i);
+                res.addAll(dfs(adjMatrix, i, visited));
+            }
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        Integer[][] adjMatrix=  new Integer[5][5];
+        Arrays.fill(adjMatrix[0], 0);
+        Arrays.fill(adjMatrix[1], 0);
+        Arrays.fill(adjMatrix[2], 0);
+        Arrays.fill(adjMatrix[3], 0);
+        Arrays.fill(adjMatrix[4], 0);
+
+        adjMatrix[0][1] = 1;
+        adjMatrix[1][2] = 1;
+        adjMatrix[2][3] = 1;
+        adjMatrix[3][4] = 1;
+
+        System.out.println(findEdge(adjMatrix, 2, 3));
+        System.out.println(dfs(adjMatrix, 1, new HashSet<>()));
+    }
+}
