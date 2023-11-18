b922e3eb0f0f3b4587df290ffc6e8d8fbf33fa7e
JessySnow <jessysnow12345@gmail.com>
2023-11-18 15:08:29 +0800

    feat: 0/1 BFS algorithm
    need a demo for 0/1 BFS

---
+++ graph/src/acgn/jessysnow/algorithm/DequeBFS.java
@@ -0,0 +1,8 @@
+package acgn.jessysnow.algorithm;
+
+/**
+ * 双端队列 BFS，用于在边权为 0/1 的图上求最短路
+ * <a href="https://leetcode.com/problems/minimum-obstacle-removal-to-reach-corner/">LeetCode</a>
+ */
+public class DequeBFS {
+}
